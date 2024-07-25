46c457418fcfad7d255afd3703be800d654f81ca
BelalBasal <basdibrahim@gmail.com>
2024-07-25 11:10:28 +0300

    v06

--- C42-G04-OOP04/Program.cs
+++ C42-G04-OOP04/Program.cs
@@ -12,12 +12,35 @@
             {
             if (complex is not null)
             {
-                // return new Complex { real = complex.real + 1, imaginary = complex.imaginary }; دي طريقة والي تحت طريقة الاتنين نفس الحاجة 
-                complex.real++;
-                return complex;
+                 return new Complex { real = complex.real + 1, imaginary = complex.imaginary }; /// طريقة والي تحت طريقة الاتنين نفس الحاجة 
+                
             }
             return complex;
             }
+        public static bool operator< (Complex left, Complex right)
+        {
+            
+            
+                if(left.real < right.real)
+                    return true;
+
+                else
+                    return false;
+
+            
+            
+        }
+        public static bool operator >(Complex left, Complex right)
+        {
+            
+            
+                if (left.real > right.real)
+                    return true;
+                else 
+                    return false;
+            
+            
+        }
         public override string ToString()
         {
             return $"{real} + {imaginary}i";
@@ -37,7 +60,7 @@
             Complex c03 = new Complex();
             c03 = c01 + c02;
             Console.WriteLine(c03);
-            Console.WriteLine(++c01);
+            Console.WriteLine(c02>c01);
         }
     }
 }
