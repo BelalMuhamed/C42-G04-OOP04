5c424371678f10f207aa9eec730a64b6bfafd6b9
BelalBasal <basdibrahim@gmail.com>
2024-07-25 15:52:34 +0300

    FirstProject

--- C42-G04-OOP04/Program.cs
+++ C42-G04-OOP04/Program.cs
@@ -1,10 +1,116 @@
 ﻿namespace C42_G04_OOP04
 {
-    internal class Program
+    internal class Point3D : IComparable,ICloneable
     {
-        static void Main(string[] args)
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Point3D(double x, double y, double z)
         {
-          
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Point3D() { }
+        //override operators == and !=
+        public static bool operator ==(Point3D a, Point3D b)
+        {
+            if(a.X==b.X && a.Y == b.Y && a.Z==b.Z) return true;
+            else return false;
+        }
+        public static bool operator !=(Point3D a, Point3D b)
+        {
+            if (a.X != b.X && a.Y != b.Y && a.Z != b.Z) return true;
+            else return false;
+        }
+        //icomparefunction 
+        public int CompareTo(object? obj)
+        {
+            Point3D point =(Point3D)obj;
+            if(this.X>point.X)return 1;
+            else if (this.X < point.X) return -1;
+            else 
+            {
+                if (this.Y > point.Y) return 1;
+                else if (this.Y < point.Y) return -1;
+                else
+                {
+                    return 0;
+                }
+
+            }
+        }
+        public override string ToString()
+        {
+            return $"Point cordinates is:({X},{Y},{Z})";
+        }
+
+        public object Clone()
+        {
+           return new Point3D(X=this.X,Y=this.Y,Z=this.Z);  
+        }
+    }
+   
+        internal class Program
+    {
+
+        static double TkeCoordinate()
+        {
+            double coordinate;
+           
+            while (true)
+            {
+                bool flag = double.TryParse(Console.ReadLine(), out coordinate);
+                if (flag)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("invalid value ");
+                }
+            }
+            return coordinate;
+        }
+        static Point3D readpoint (Point3D point)
+        {
+            Console.WriteLine("enter coordinate x ");
+            point.X = TkeCoordinate();
+            Console.WriteLine("enter coordinate y");
+            point.Y = TkeCoordinate();
+            Console.WriteLine("enter coordinate z");
+
+            point.Z = TkeCoordinate();
+            return point;
+        }
+    static void Main(string[] args)
+        {
+            Point3D point3D = new Point3D(10, 10, 10);
+            Console.WriteLine(point3D);
+            Point3D p01 = new Point3D();
+            Point3D p02 = new Point3D();
+            readpoint(p01);
+            readpoint(p02);
+            Console.WriteLine(p01);
+            Console.WriteLine(p02);
+            Console.WriteLine(p01==p02);//override the operator to can compare between two points
+            Point3D[] points = new Point3D[]
+        {
+            new Point3D(3, 2, 7),
+            new Point3D(1, 3, 6),
+            new Point3D(7, 2, 5),
+            new Point3D(0, 0, 0),
+            p01,
+            p02
+        };
+            Array.Sort(points);
+            foreach (Point3D item in points)
+            {
+                Console.WriteLine(item);
+            }
+           Point3D p03= (Point3D)p02.Clone();
+            Console.WriteLine(p03);
         }
     }
 }
